21b57c5215b2c1a3c3b9bcbdcb2167ad87ee65e3
Dabz <itzdabbzz@gmail.com>
2025-05-17 21:29:39 -0500

    Update Rooms Page Table Names

--- src/electron/renderer/features/rooms/components/InteriorRoomAudioGameDataList/index.tsx
+++ src/electron/renderer/features/rooms/components/InteriorRoomAudioGameDataList/index.tsx
@@ -10,7 +10,7 @@ import { parseHexToString } from '@/electron/renderer/utils';
 
 import { updateInteriorRoomAudioGameData } from '../../index';
 
-import { InputWrapper, MediumInput, SmallInput } from './styles';
+import { InputWrapper, MediumInput, SmallInput, ScrollContainer } from './styles';
 
 export const InteriorRoomAudioGameDataList = (): JSX.Element => {
   const { interior, fetchInterior } = useInterior();
@@ -37,165 +37,167 @@ export const InteriorRoomAudioGameDataList = (): JSX.Element => {
   };
 
   return (
-    <TableContainer>
-      <Table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Flags</th>
-            <th>MloRoom</th>
-            <th>Zone</th>
-            <th>Unk02</th>
-            <th>Unk03</th>
-            <th>Reverb</th>
-            <th>Echo</th>
-            <th>Sound</th>
-            <th>Unk07</th>
-            <th>Unk08</th>
-            <th>Unk09</th>
-            <th>Unk10</th>
-            <th>Unk11</th>
-            <th>Unk12</th>
-            <th>Unk13</th>
-            <th>SoundSet</th>
-          </tr>
-        </thead>
-        <tbody>
-          {interiorRoomAudioGameDataList.map((interiorRoomAudioGameData, index) => (
-            <tr key={index}>
-              <td>{interiorRoomAudioGameData.name}</td>
-              <td>{parseHexToString(interiorRoomAudioGameData.flags)}</td>
-              <td>{interiorRoomAudioGameData.mloRoom}</td>
-              <td>
-                <InputWrapper>
-                  <MediumInput
-                    value={interiorRoomAudioGameData.zone}
-                    type="text"
-                    onChange={e => update(index, { zone: e.target.value })}
-                  />
-                </InputWrapper>
-              </td>
-              <td>
-                <InputWrapper>
-                  <SmallInput
-                    value={interiorRoomAudioGameData.unk02}
-                    type="number"
-                    onChange={e => update(index, { unk02: Number(e.target.value) })}
-                  />
-                </InputWrapper>
-              </td>
-              <td>
-                <InputWrapper>
-                  <SmallInput
-                    value={interiorRoomAudioGameData.unk03}
-                    type="number"
-                    step={0.05}
-                    onChange={e => update(index, { unk03: Number(e.target.value) })}
-                  />
-                </InputWrapper>
-              </td>
-              <td>
-                <InputWrapper>
-                  <SmallInput
-                    value={interiorRoomAudioGameData.reverb}
-                    type="number"
-                    min={0}
-                    max={1}
-                    step={0.1}
-                    onChange={e => update(index, { reverb: Number(e.target.value) })}
-                  />
-                </InputWrapper>
-              </td>
-              <td>
-                <InputWrapper>
-                  <SmallInput
-                    value={interiorRoomAudioGameData.echo}
-                    type="number"
-                    min={0}
-                    max={1}
-                    step={0.1}
-                    onChange={e => update(index, { echo: Number(e.target.value) })}
-                  />
-                </InputWrapper>
-              </td>
-              <td>
-                <InputWrapper>
-                  <MediumInput
-                    value={interiorRoomAudioGameData.sound}
-                    type="text"
-                    onChange={e => update(index, { sound: e.target.value })}
-                  />
-                </InputWrapper>
-              </td>
-              <td>
-                <InputWrapper>
-                  <SmallInput
-                    value={interiorRoomAudioGameData.unk07}
-                    type="number"
-                    onChange={e => update(index, { unk07: Number(e.target.value) })}
-                  />
-                </InputWrapper>
-              </td>
-              <td>
-                <InputWrapper>
-                  <SmallInput
-                    value={interiorRoomAudioGameData.unk08}
-                    type="number"
-                    onChange={e => update(index, { unk08: Number(e.target.value) })}
-                  />
-                </InputWrapper>
-              </td>
-              <td>
-                <InputWrapper>
-                  <SmallInput
-                    value={interiorRoomAudioGameData.unk09}
-                    type="number"
-                    onChange={e => update(index, { unk09: Number(e.target.value) })}
-                  />
-                </InputWrapper>
-              </td>
-              <td>
-                <InputWrapper>
-                  <SmallInput
-                    value={interiorRoomAudioGameData.unk10}
-                    type="number"
-                    step={0.1}
-                    onChange={e => update(index, { unk10: Number(e.target.value) })}
-                  />
-                </InputWrapper>
-              </td>
-              <td>
-                <InputWrapper>
-                  <SmallInput
-                    value={interiorRoomAudioGameData.unk11}
-                    type="number"
-                    onChange={e => update(index, { unk11: Number(e.target.value) })}
-                  />
-                </InputWrapper>
-              </td>
-              <td>
-                <InputWrapper>
-                  <SmallInput
-                    value={interiorRoomAudioGameData.unk12}
-                    type="number"
-                    onChange={e => update(index, { unk12: Number(e.target.value) })}
-                  />
-                </InputWrapper>
-              </td>
-              <td>
-                <InputWrapper>
-                  <MediumInput
-                    value={interiorRoomAudioGameData.unk13}
-                    type="text"
-                    onChange={e => update(index, { unk13: e.target.value })}
-                  />
-                </InputWrapper>
-              </td>
-              <td>{interiorRoomAudioGameData.soundSet}</td>
+    <ScrollContainer>
+      <TableContainer>
+        <Table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Flags</th>
+              <th>RoomName</th>
+              <th>AmbientZone</th>
+              <th>InteriorType</th>
+              <th>ReverbSmall</th>
+              <th>ReverbMedium</th>
+              <th>ReverbLarge</th>
+              <th>RoomToneSound</th>
+              <th>RainType</th>
+              <th>ExteriorAudibility</th>
+              <th>RoomOcclusionDamping</th>
+              <th>NonMarkedPortalOcclusion</th>
+              <th>DistanceFromPortalForOcclusion</th>
+              <th>DistanceFromPortalFadeDistance</th>
+              <th>WeaponMetrics</th>
+              <th>InteriorWallaSoundSet</th>
             </tr>
-          ))}
-        </tbody>
-      </Table>
-    </TableContainer>
+          </thead>
+          <tbody>
+            {interiorRoomAudioGameDataList.map((interiorRoomAudioGameData, index) => (
+              <tr key={index}>
+                <td>{interiorRoomAudioGameData.name}</td>
+                <td>{parseHexToString(interiorRoomAudioGameData.flags)}</td>
+                <td>{interiorRoomAudioGameData.mloRoom}</td>
+                <td>
+                  <InputWrapper>
+                    <MediumInput
+                      value={interiorRoomAudioGameData.zone}
+                      type="text"
+                      onChange={e => update(index, { zone: e.target.value })}
+                    />
+                  </InputWrapper>
+                </td>
+                <td>
+                  <InputWrapper>
+                    <SmallInput
+                      value={interiorRoomAudioGameData.unk02}
+                      type="number"
+                      onChange={e => update(index, { unk02: Number(e.target.value) })}
+                    />
+                  </InputWrapper>
+                </td>
+                <td>
+                  <InputWrapper>
+                    <SmallInput
+                      value={interiorRoomAudioGameData.unk03}
+                      type="number"
+                      step={0.05}
+                      onChange={e => update(index, { unk03: Number(e.target.value) })}
+                    />
+                  </InputWrapper>
+                </td>
+                <td>
+                  <InputWrapper>
+                    <SmallInput
+                      value={interiorRoomAudioGameData.reverb}
+                      type="number"
+                      min={0}
+                      max={1}
+                      step={0.1}
+                      onChange={e => update(index, { reverb: Number(e.target.value) })}
+                    />
+                  </InputWrapper>
+                </td>
+                <td>
+                  <InputWrapper>
+                    <SmallInput
+                      value={interiorRoomAudioGameData.echo}
+                      type="number"
+                      min={0}
+                      max={1}
+                      step={0.1}
+                      onChange={e => update(index, { echo: Number(e.target.value) })}
+                    />
+                  </InputWrapper>
+                </td>
+                <td>
+                  <InputWrapper>
+                    <MediumInput
+                      value={interiorRoomAudioGameData.sound}
+                      type="text"
+                      onChange={e => update(index, { sound: e.target.value })}
+                    />
+                  </InputWrapper>
+                </td>
+                <td>
+                  <InputWrapper>
+                    <SmallInput
+                      value={interiorRoomAudioGameData.unk07}
+                      type="number"
+                      onChange={e => update(index, { unk07: Number(e.target.value) })}
+                    />
+                  </InputWrapper>
+                </td>
+                <td>
+                  <InputWrapper>
+                    <SmallInput
+                      value={interiorRoomAudioGameData.unk08}
+                      type="number"
+                      onChange={e => update(index, { unk08: Number(e.target.value) })}
+                    />
+                  </InputWrapper>
+                </td>
+                <td>
+                  <InputWrapper>
+                    <SmallInput
+                      value={interiorRoomAudioGameData.unk09}
+                      type="number"
+                      onChange={e => update(index, { unk09: Number(e.target.value) })}
+                    />
+                  </InputWrapper>
+                </td>
+                <td>
+                  <InputWrapper>
+                    <SmallInput
+                      value={interiorRoomAudioGameData.unk10}
+                      type="number"
+                      step={0.1}
+                      onChange={e => update(index, { unk10: Number(e.target.value) })}
+                    />
+                  </InputWrapper>
+                </td>
+                <td>
+                  <InputWrapper>
+                    <SmallInput
+                      value={interiorRoomAudioGameData.unk11}
+                      type="number"
+                      onChange={e => update(index, { unk11: Number(e.target.value) })}
+                    />
+                  </InputWrapper>
+                </td>
+                <td>
+                  <InputWrapper>
+                    <SmallInput
+                      value={interiorRoomAudioGameData.unk12}
+                      type="number"
+                      onChange={e => update(index, { unk12: Number(e.target.value) })}
+                    />
+                  </InputWrapper>
+                </td>
+                <td>
+                  <InputWrapper>
+                    <MediumInput
+                      value={interiorRoomAudioGameData.unk13}
+                      type="text"
+                      onChange={e => update(index, { unk13: e.target.value })}
+                    />
+                  </InputWrapper>
+                </td>
+                <td>{interiorRoomAudioGameData.soundSet}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </TableContainer>
+    </ScrollContainer>
   );
 };
--- src/electron/renderer/features/rooms/components/InteriorRoomAudioGameDataList/styles.ts
+++ src/electron/renderer/features/rooms/components/InteriorRoomAudioGameDataList/styles.ts
@@ -21,3 +21,11 @@ export const MediumInput = styled(StyledInput)`
 export const SmallInput = styled(StyledInput)`
   min-width: 56px;
 `;
+
+export const ScrollContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  overflow-x: auto;
+  overflow-y: auto;
+  scrollbar-gutter: stable both-edges;
+`;
